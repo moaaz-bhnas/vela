e2b6aff3d5faf7ce7e8e7adc2cba5e23f7078fe6
Moaaz Bhnas <moaaz.bhnas@orchestrapay.com>
2026-01-31 09:51:13 +0200

    Small fix

--- storefront/src/modules/layout/components/desktop-side-menu/index.tsx
+++ storefront/src/modules/layout/components/desktop-side-menu/index.tsx
@@ -4,11 +4,11 @@ import { HttpTypes } from "@medusajs/types"
 import SideMenu from "../side-menu"
 import { useNavScroll } from "../nav-scroll-wrapper"
 
-type MenuIconButtonProps = {
+type DesktopSideMenuProps = {
   regions: HttpTypes.StoreRegion[] | null
 }
 
-const DesktopSideMenu = ({ regions }: MenuIconButtonProps) => {
+const DesktopSideMenu = ({ regions }: DesktopSideMenuProps) => {
   const isScrolled = useNavScroll()
 
   return (
