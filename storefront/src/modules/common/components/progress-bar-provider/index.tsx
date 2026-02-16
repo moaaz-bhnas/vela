888ce207e732db7cd44fe550762954a4fd1168de
Moaaz Bhnas <moaaz.bhnas@orchestrapay.com>
2026-02-16 17:36:49 +0200

    Small change

--- storefront/src/modules/common/components/progress-bar-provider/index.tsx
+++ storefront/src/modules/common/components/progress-bar-provider/index.tsx
@@ -5,7 +5,7 @@ import { AppProgressProvider } from "@bprogress/next"
 const NProgressProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <AppProgressProvider
-      height="2px"
+      height="2.5px"
       color="var(--bg-interactive)"
       options={{ showSpinner: false }}
       shallowRouting
