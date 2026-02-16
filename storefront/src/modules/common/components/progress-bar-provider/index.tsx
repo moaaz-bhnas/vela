d19349641be54b20882c5340d23ac30d18759899
Moaaz Bhnas <moaaz.bhnas@orchestrapay.com>
2026-02-16 17:32:52 +0200

    Added progressbar

---
+++ storefront/src/modules/common/components/progress-bar-provider/index.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { AppProgressProvider } from "@bprogress/next"
+
+const NProgressProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <AppProgressProvider
+      height="2px"
+      color="var(--bg-interactive)"
+      options={{ showSpinner: false }}
+      shallowRouting
+    >
+      {children}
+    </AppProgressProvider>
+  )
+}
+
+export default NProgressProvider
